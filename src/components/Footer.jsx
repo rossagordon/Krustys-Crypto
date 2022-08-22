4b5d14507d36a0c6ccf4fec496a4df4f54e63374
Ross Gordon <rossagordonstl@gmail.com>
2022-08-22 11:19:26 -0400

    Footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[]'>
+    <div className='bg-[#00957C] h-24 m-w-[1240px] text-black'>
+      <h5 className='text-center pt-8 font-bold'>&copy; Krusty's Crypto Tracker 2022</h5>
     </div>
   )
 }
